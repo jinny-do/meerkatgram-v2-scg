0def8af9e2ebfc63409630880506b21cd8ba04ab
jinny-do <sonmojiri7@gmail.com>
2026-07-23 17:37:06 +0900

    add ExceptionHandler & OpenAPI & JWTParser & Auth Service Routing

---
+++ src/main/java/com/meerkatgramv2scg/global/error/custom/InvalidTokenException.java
@@ -0,0 +1,7 @@
+package com.meerkatgramv2scg.global.error.custom;
+
+public class InvalidTokenException extends RuntimeException {
+    public InvalidTokenException(String message) {
+        super(message);
+    }
+}
